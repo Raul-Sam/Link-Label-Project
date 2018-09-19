d68009ee5412b0224ea933cb142346b1bc15d507
Raul <rharo1@dtcc.edu>
2018-09-19 10:18:26 -0400

    Thinig

--- Link Label Demonstration/Link Label Demonstration/Form1.Designer.cs
+++ Link Label Demonstration/Link Label Demonstration/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.lnkTest = new System.Windows.Forms.LinkLabel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // lnkTest
@@ -41,11 +42,19 @@
             this.lnkTest.TabStop = true;
             this.lnkTest.Text = "This is a test label";
             // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(213, 117);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(393, 341);
+            this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.lnkTest);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.LinkLabel lnkTest;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
 
